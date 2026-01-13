f0fadd727c28887bb7bf624e0ae235e277a75811
jessicachen950 <jc123@wellesley.edu>
2026-01-13 10:42:18 -0500

    ask user for favorite fruit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 fruits = ["apple", "banana","cantaloupe","dragonfruit"]
 for fruit in fruits:
     print(fruit)
+
+user_fruit = input("What is your favorite fruit? ")
+if user_fruit in fruits:
+    print("Good job, that is a fruit.")
+else:
+    print("sorry, that is not a fruit. there are only 4 fruits: " + str(fruits))
